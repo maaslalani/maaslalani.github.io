52a4f893c114dc7ed012582607573cd49b67e157
Maas Lalani <maaslalani1@gmail.com>
2019-01-29 17:58:19 -0500

    Remove non-english character sets

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const GREETINGS = ['Hello', 'Hey', 'Hi', 'Hola', 'Bonjour', 'Welcome', '你好', 'ہیلو']
+const GREETINGS = ['Hello', 'Hey', 'Hi', 'Hola', 'Bonjour', 'Welcome']
 
 // Display a random greeting
 document.getElementById('greeting').innerHTML = GREETINGS[Math.floor(Math.random() * GREETINGS.length)]
